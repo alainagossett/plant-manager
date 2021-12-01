3ba0d4a5d0651fbe3aca82581a3d18a093046f59
Alaina <alaina.gossett@gmail.com>
2021-12-01 16:43:23 -0700

    adds plant model

--- models/plant.js
+++ models/plant.js
@@ -5,8 +5,12 @@ const Schema = mongoose.Schema;
 
 //Define Schema
 const plantSchema = new Schema({
-    
-})
+    name: { type: String, required: true },
+    colors: String,
+    sunlight: String,
+    water: String,
+    pests: String
+}, { timestamps: true });
 
 //Compile mongoose Schema into a model
 const Plant = mongoose.model("Plant", plantSchema)
