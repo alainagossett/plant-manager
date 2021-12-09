d4a06571c6ef428fe7ce6ce6d0a5770af2eb36ca
Alaina <alaina.gossett@gmail.com>
2021-12-08 18:58:14 -0700

    fixes index button alignment

--- controllers/users.js
+++ controllers/users.js
@@ -1,2 +1,9 @@
 const usersRouter = require('express').Router();
 const User = require('../models/user');
+
+const bcrypt = require('bcrypt')
+const SALT_ROUNDS = 10;
+
+usersRouter.get('/login', (req, res) => {
+    res.render('login.ejs', {err: ''});
+});
