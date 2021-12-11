3bb4f8a157233b769f715bceb7d28e3a2d9ca0cd
Alaina <alaina.gossett@gmail.com>
2021-12-11 10:42:08 -0700

    cleans up some more code

--- models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const userSchema = new Schema({
-    email: String,
-    password: String,
-}, { timestamps: true });
-
-module.exports = mongoose.model('User', userSchema);
